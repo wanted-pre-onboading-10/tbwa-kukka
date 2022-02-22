709fc51f0c9d2d3e25b7dbbeab7d32fd7382c401
wook95 <uclee95@gmail.com>
2022-02-22 18:17:02 +0900

    add: 반응형 배너 캐러셀 구현

--- js/banner.js
+++ js/banner.js
@@ -3,3 +3,6 @@ $('.banner__container').css('height',$('.banner__item--left').width()*0.28);
 $(window).resize(function(){
   $('.banner__container').css('height',$('.banner__item--left').width()*0.28)
 })
+$('.carousel').carousel({
+  interval: 0
+})
